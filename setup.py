66f636832f20b0c977bf53b2a396fd26c22460a4
lq0000 <root@lo.calho.st>
2021-06-14 18:55:51 -0600

    Bugfix: forgot to package the drivers submodule

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="travis mick",
     author_email="root@lo.calho.st",
     description="simple dependency manager for sourcemod projects",
-    packages=['sourceknight'],
+    packages=['sourceknight', 'sourceknight.drivers'],
     python_requires='>=3.8',
     install_requires=['pyyaml>=5.4,<6', 'GitPython>=3.1,<4', 'requests>=2.25,<3'],
     entry_points={'console_scripts': ["sourceknight=sourceknight.main:main"]}
